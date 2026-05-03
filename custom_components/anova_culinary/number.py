9d2c2fba06cbda98ec4566bc4d48c6a4136837b5
Alex Groleau <agroleau@teampalmtree.com>
2026-05-03 14:36:28 -0400

    Revert "cleanup"
    This reverts commit 458e93474bf1faebb1493bf8ff7f2bee3057e335.

--- custom_components/anova_culinary/number.py
+++ custom_components/anova_culinary/number.py
@@ -26,6 +26,7 @@ async def async_setup_entry(
         if device.product == AnovaProduct.APO:
             entities.extend([
                 AnovaSteam(client, device),
+                AnovaTimer(client, device),
             ])
             
     async_add_entities(entities)
@@ -81,4 +82,59 @@ class AnovaSteam(NumberEntity):
         if cook and cook.current_stage:
             cook.current_stage.steam = int(value)
             await self._client.play_cook(self._device.id, cook)
+
+class AnovaTimer(NumberEntity):
+    """Timer duration slider in minutes."""
+
+    _attr_has_entity_name = True
+    _attr_name = "Timer"
+    _attr_icon = "mdi:timer-outline"
+    _attr_native_min_value = 0
+    _attr_native_max_value = 1440
+    _attr_native_step = 1
+
+    def __init__(self, client: AnovaClient, device: AnovaDevice) -> None:
+        self._client = client
+        self._device = device
+        self._attr_unique_id = f"{DOMAIN}_{self._device.id}_timer"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, self._device.id)},
+            name=self._device.name,
+            manufacturer=MANUFACTURER,
+            model=self._device.model,
+        )
+        self._remove_cb = None
+
+    async def async_added_to_hass(self) -> None:
+        self._remove_cb = self._client.register_callback(self._handle_update)
+        self._handle_update(self._device.id)
+
+    async def async_will_remove_from_hass(self) -> None:
+        if self._remove_cb:
+            self._remove_cb()
+
+    @callback
+    def _handle_update(self, device_id: str) -> None:
+        if device_id != self._device.id: return
+        state = self._client.get_apo_state(device_id)
+        if not state or not state.cook: return
+        self._attr_available = state.is_running
+        
+        try:
+            curr_stage = state.cook.current_stage
+            if curr_stage:
+                if isinstance(curr_stage.advance, AnovaPOTimer):
+                    self._attr_native_value = int(curr_stage.advance.duration / 60.0) # show minutes
+                else:
+                    self._attr_native_value = 0
+        except: pass
+        self.async_write_ha_state()
+
+    async def async_set_native_value(self, value: float) -> None:
+        cook = self._client.get_current_cook(self._device.id)
+        if cook and cook.current_stage:
+            # We assume a manual trigger for simple slider adjustments!
+            cook.current_stage.advance = AnovaPOTimer(duration=int(value * 60), trigger=AnovaPOTimerTrigger.MANUALLY)
+            await self._client.play_cook(self._device.id, cook)
+
 
