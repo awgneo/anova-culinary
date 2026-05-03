88376c8f29bec7662045ddc3edd73484e0cc87aa
Alex Groleau <agroleau@teampalmtree.com>
2026-05-03 15:51:59 -0400

    Use ha-icon for text-box-outline in recipe editor header

--- custom_components/anova_culinary/www/panel.js
+++ custom_components/anova_culinary/www/panel.js
@@ -491,7 +491,7 @@ class AnovaCulinary extends LitElement {
         <div class="toolbar">
            <div class="toolbar-title">
              <div class="type-icon" style="margin-right:16px;">
-                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM12 16v-4M12 8h.01"/></svg>
+                 <ha-icon icon="mdi:text-box-outline"></ha-icon>
              </div>
              ${!this.editingRecipe.id ? "Create Recipe" : "Edit Recipe"}
            </div>
